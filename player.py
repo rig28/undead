ac32d270458b130c5f35ffe2bc962ae4d6a12641
modeler :D <liam.beeson13@outlook.com>
2025-09-18 09:45:52 +1000

    Create player.py

---
+++ player.py
@@ -0,0 +1,15 @@
+import pygame
+import pyautogui
+
+pygame.init()
+
+    screen_width = 800
+    screen_height = 600
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    pygame.display.set_caption("Start")
+
+start = "start"
+
+player_hp = 100
+
+print(player_hp
